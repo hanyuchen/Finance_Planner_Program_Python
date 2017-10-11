0e22ff2a7f77c78ce3467a9fd1974b1e4c6c4470
Ayo Animashaun <ayobamidelea@gmail.com>
2017-10-10 22:58:22 -0700

    Roles

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ d1=['Ayo','Hanyu','Muxuan','Yucheng']*25
 d2=['ayoanimashaun@berkeley.edu','hanyu_chen@berkeley.edu','muxuan_lyu@berkeley.edu','yucheng.lin@berkeley.edu']*25
 d3=d1
 d4=['Ayospassword12@','Hhanyuspassword12@','Muxuansappasword12@','Yuchengspassword12@']*25
-i1=[10000,10000,10000,10000]*25
+i1=[1000,1200,5000,7000]*25
 np.random.seed(0)
 e1=list(np.random.randint(100,150,size=100))
 e2=list(np.random.randint(100,150,size=100))
@@ -22,15 +22,9 @@ e6=list(np.random.randint(100,150,size=100))
 e7=list(np.random.randint(100,150,size=100))
 e8=list(np.random.randint(100,150,size=100))
 e9=list(np.random.randint(100,150,size=100))
-b1=list(np.random.randint(150,180,size=100))
-b2=list(np.random.randint(150,180,size=100))
-b3=list(np.random.randint(150,180,size=100))
-b4=list(np.random.randint(150,180,size=100))
-b5=list(np.random.randint(150,180,size=100))
-b6=list(np.random.randint(150,180,size=100))
-b7=list(np.random.randint(150,180,size=100))
-b8=list(np.random.randint(150,180,size=100))
-b9=list(np.random.randint(150,180,size=100))
+b1=i1=[900,900,3000,5000]*25
+
+
 
 Large_d={'User Name':d1,'Email Address':d2,'Name':d3}
 
@@ -47,14 +41,16 @@ df['TravelE']=e6
 df['LoansE']=e7
 df['House&BillsE']=e8
 df['OthersE']=e9
-df['GroceriesB']=b1
-df['RestaurantsB']=b2 
-df['ClothingB']=b3
-df['EntertainmentB']=b4
-df['E-devices']=b5
-df['Travel']=b6 
-df['Loans']=b7
-df['House&Bills']=b8
-df['Others']=b9
+df['Budget']=b1
+
+df['GroceriesE']=(df['GroceriesE']).groupby(df['Name']).cumsum()
+df['RestaurantsE']=(df['RestaurantsE']).groupby(df['Name']).cumsum()
+df['ClothingE']=(df['ClothingE']).groupby(df['Name']).cumsum()
+df['EntertainmentE']=(df['EntertainmentE']).groupby(df['Name']).cumsum()
+df['E-devicesE']=(df['E-devicesE']).groupby(df['Name']).cumsum()
+df['TravelE']=(df['TravelE']).groupby(df['Name']).cumsum()
+df['LoansE']=(df['LoansE']).groupby(df['Name']).cumsum()
+df['House&BillsE']=(df['House&BillsE']).groupby(df['Name']).cumsum()
+df['OthersE']=(df['OthersE']).groupby(df['Name']).cumsum()
 
 df.to_csv('fake_database.csv', sep=',', index=False)
