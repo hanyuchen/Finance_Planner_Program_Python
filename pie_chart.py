c1c4e8b663453c5ea757a87b258e1856fd5f319d
Han-Yu Chen <okashi0083.tem97@g2.nctu.edu.tw>
2017-10-16 12:24:24 -0700

    Update pie_chart.py

--- pie_chart.py
+++ pie_chart.py
@@ -20,7 +20,7 @@ def pie_chart():
     #make pie chart
     labels = category
     sizes = amount
-    explode = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    explode = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) 
 
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
